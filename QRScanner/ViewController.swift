d34bfbf4db9e6d61149f12006877f65b9e37a1e4
islam kirenli <islamkirenli@islam-Air.home>
2024-04-15 23:33:05 +0300

    Project Open

---
+++ QRScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRScanner
+//
+//  Created by islam kirenli on 15.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
