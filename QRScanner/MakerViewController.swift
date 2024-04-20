577506051c81b5dd1df2794dff7db0e476185394
islam kirenli <islamkirenli@islam-Air.home>
2024-04-20 14:57:56 +0300

    URL QR maker completed.

--- QRScanner/MakerViewController.swift
+++ QRScanner/MakerViewController.swift
@@ -1,29 +1,59 @@
-//
-//  MakerViewController.swift
-//  QRScanner
-//
-//  Created by islam kirenli on 16.04.2024.
-//
-
 import UIKit
 
 class MakerViewController: UIViewController {
 
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(klavyeKapat))
+        view.addGestureRecognizer(gestureRecognizer)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func klavyeKapat(){
+        view.endEditing(true)
     }
-    */
+    
+    
+    @IBAction func createQRCodeButtonTapped(_ sender: Any) {
+        guard let urlString = urlTextField.text, !urlString.isEmpty else {
+            // Kullanıcı URL girmeden butona tıklarsa hata mesajı gösterin
+            showAlert(message: "Lütfen bir URL girin")
+            return
+        }
+        
+        if let qrCodeImage = generateQRCode(from: urlString) {
+            // QR kodunu imageView'a atayın
+            imageView.image = qrCodeImage
+        } else {
+            // QR kodu oluşturulamazsa hata mesajı gösterin
+            showAlert(message: "QR kodu oluşturulamadı")
+        }
+    }
+    
+    
 
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 10, y: 10)
+            
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+        return nil
+    }
+
+    func showAlert(message: String) {
+        let alertController = UIAlertController(title: "Hata", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Tamam", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
+
+
